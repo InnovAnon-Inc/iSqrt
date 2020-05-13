1339898da17fd8799e1a95ee76723c3854781132
Innovations Anonymous <InnovAnon-Inc@protonmail.com>
2020-05-12 19:31:30 -0500

    bugfix

---
+++ confdefs.h
@@ -0,0 +1,24 @@
+/* confdefs.h */
+#define PACKAGE_NAME "libisqrt"
+#define PACKAGE_TARNAME "libisqrt"
+#define PACKAGE_VERSION "0.0"
+#define PACKAGE_STRING "libisqrt 0.0"
+#define PACKAGE_BUGREPORT "laurence.a.maddox@gmail.com"
+#define PACKAGE_URL ""
+#define PACKAGE "libisqrt"
+#define VERSION "0.0"
+#define STDC_HEADERS 1
+#define HAVE_SYS_TYPES_H 1
+#define HAVE_SYS_STAT_H 1
+#define HAVE_STDLIB_H 1
+#define HAVE_STRING_H 1
+#define HAVE_MEMORY_H 1
+#define HAVE_STRINGS_H 1
+#define HAVE_INTTYPES_H 1
+#define HAVE_STDINT_H 1
+#define HAVE_UNISTD_H 1
+#define HAVE_DLFCN_H 1
+#define LT_OBJDIR ".libs/"
+#define HAVE_STDLIB_H 1
+#define HAVE_MATH_H 1
+#define HAVE_GLITTER_H 1
